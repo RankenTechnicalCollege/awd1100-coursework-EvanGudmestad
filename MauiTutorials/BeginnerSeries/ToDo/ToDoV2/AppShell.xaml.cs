6b76b9164329dcb2422dd7ece1348d9bff52f609
EvanGudmestad <eagudmestad@ranken.edu>
2025-05-21 14:19:11 -0500

    ToDo UI

---
+++ MauiTutorials/BeginnerSeries/ToDo/ToDoV2/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDoV2
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MauiTutorials/BeginnerSeries/ToDo/ToDoV2/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace ToDoV2
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+       
+    }
+}
