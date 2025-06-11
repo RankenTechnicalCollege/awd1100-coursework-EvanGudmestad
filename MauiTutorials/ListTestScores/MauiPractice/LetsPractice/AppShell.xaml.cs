7d252ba59b178f4d8832ea1cd53813c11fadcfdd
EvanGudmestad <eagudmestad@ranken.edu>
2025-06-11 11:19:30 -0500

    List of Test Scores Maui App

---
+++ MauiTutorials/ListTestScores/MauiPractice/LetsPractice/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LetsPractice
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MauiTutorials/ListTestScores/MauiPractice/LetsPractice/MainPage.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.ObjectModel;
+
+namespace LetsPractice
+{
+    public partial class MainPage : ContentPage
+    {
+        public ObservableCollection<int> TestScores { get; set; } = new ObservableCollection<int> { 85, 92, 76, 88, 95 };
+        int? selectedScoreIndex = null;
+
+        public int? MaxScore
+        {
+            get => TestScores.Count > 0 ? TestScores.Max() : null;
+        }
+        public int? MinScore
+        {
+            get => TestScores.Count > 0 ? TestScores.Min() : null;
+        }
+        public double? AverageScore
+        {
+            get => TestScores.Count > 0 ? TestScores.Average() : null;
+        }
+
+        private void UpdateStatistics()
+        {
+            OnPropertyChanged("MaxScore");
+            OnPropertyChanged("MinScore");
+            OnPropertyChanged("AverageScore");
+        }
+
+        public bool IsUpdateMode
+        {
+            get => updateMode;
+            set
+            {
+                if (updateMode != value)
+                {
+                    updateMode = value;
+                    OnPropertyChanged("IsUpdateMode");
+                }
+            }
+        }
+        private bool updateMode = false;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+            TestScores.CollectionChanged += (s, e) => UpdateStatistics();
+        }
+
+        private void OnAddClicked(object sender, EventArgs e)
+        {
+            if (int.TryParse(ScoreEntry.Text, out int newScore))
+            {
+                TestScores.Add(newScore);
+                ScoreEntry.Text = string.Empty;
+            }
+            UpdateStatistics();
+        }
+
+        private void OnDeleteScore(object sender, EventArgs e)
+        {
+            if (sender is Button button && button.CommandParameter is int score)
+            {
+                TestScores.Remove(score);
+            }
+            UpdateStatistics();
+        }
+
+        private void OnScoreSelected(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.CurrentSelection.FirstOrDefault() is int selectedScore)
+            {
+                selectedScoreIndex = TestScores.IndexOf(selectedScore);
+                UpdateEntry.Text = selectedScore.ToString();
+                IsUpdateMode = true;
+            }
+        }
+
+        private void OnUpdateClicked(object sender, EventArgs e)
+        {
+            if (selectedScoreIndex.HasValue && int.TryParse(UpdateEntry.Text, out int updatedScore))
+            {
+                TestScores[selectedScoreIndex.Value] = updatedScore;
+                IsUpdateMode = false;
+                ScoresCollectionView.SelectedItem = null;
+                UpdateEntry.Text = string.Empty;
+                selectedScoreIndex = null;
+            }
+            UpdateStatistics();
+        }
+
+        private void OnCancelUpdateClicked(object sender, EventArgs e)
+        {
+            IsUpdateMode = false;
+            ScoresCollectionView.SelectedItem = null;
+            UpdateEntry.Text = string.Empty;
+            selectedScoreIndex = null;
+        }
+    }
+
+}
